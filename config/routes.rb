66921795fd8a634975dce6cf5a30249340068b46
Adrian Gutierrez Alonso <ingenieria.aga@gmail.com>
2019-09-02 23:44:17 +0200

    add sign up page first, remove navbar from sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  root to: 'pages#home'
-  resources :users, only: [:show]
+  # AG: Devise normally shows "login" page first by default
+  # and this is to do the opposite, show first "sign up". Many 
+  # social network apps do this instead.  
+  devise_scope :user do
+    authenticated :user do
+      root to: 'users#show'
+    end
+    unauthenticated :user do
+      root to: 'devise/registrations#new', as: :unauthenticated_root
+    end
+  end
+
+  # resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
